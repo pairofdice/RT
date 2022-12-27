5ec66d149f49e5483c029be8818b0003219fdb60
Max Mäkinen <quantumhound@gmail.com>
2022-12-27 11:48:37 +0200

    removed empty lines

--- src/object_new.c
+++ src/object_new.c
@@ -21,16 +21,13 @@ t_object	object_new(int shape_type)
 	new_object.up = point_new(0.0, 1.0, 0.0);
 	new_object.rot = point_new(0.0, 0.0, 0.0);
 	new_object.scale = point_new(1.0, 1.0, 1.0);
-
 	new_object.negative = FALSE;
-
 	new_object.transform = matrix_new_identity(4);
 	new_object.id = id;
 	id++;
 	new_object.loc = point_new(0, 0, 0);
 	new_object.type = shape_type;
 	new_object.material = material_new();
-
-	new_object.motion = motion_new(FALSE, 1.0, tuple_unit(vector_new(1,0,0)));
+	new_object.motion = motion_new(FALSE, 1.0, tuple_unit(vector_new(1, 0, 0)));
 	return (new_object);
 }
