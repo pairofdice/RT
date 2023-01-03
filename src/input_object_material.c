1743ee4893a5c9a7ee13d0c6edcc33c8bf6990d7
Max Mäkinen <quantumhound@gmail.com>
2023-01-03 11:08:20 +0200

    input can now read surface details. Color in its own node

---
+++ src/input_object_material.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_object.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakinen <mmakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 13:26:33 by mmakinen          #+#    #+#             */
+/*   Updated: 2022/12/13 13:35:44 by mmakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/input.h"
+
+void	get_surface(t_xml_node *surface, t_material *mat)
+{
+	char		*attr_value;
+
+	attr_value = xml_node_attr_value(surface, "ambient");
+	if (attr_value)
+		mat->ambient = ft_atof(attr_value);
+	attr_value = xml_node_attr_value(surface, "diffuse");
+	if (attr_value)
+		mat->diffuse = ft_atof(attr_value);
+	attr_value = xml_node_attr_value(surface, "specular");
+	if (attr_value)
+		mat->specular = ft_atof(attr_value);
+	attr_value = xml_node_attr_value(surface, "shininess");
+	if (attr_value)
+		mat->shininess = ft_atof(attr_value);
+	attr_value = xml_node_attr_value(surface, "reflective");
+	if (attr_value)
+		mat->reflective = ft_atof(attr_value);
+}
+
+int	get_material_details(t_xml_node *node, t_material *mat)
+{
+	int			index;
+	t_tuple		col;
+	t_xml_node	*temp;
+
+	index = 0;
+	while (index < node->children.size)
+	{
+		temp = xml_node_child(node, index);
+		if (!ft_strcmp(temp->tag, "color"))
+		{
+			get_tuple(&col, temp->data);
+			mat->color = color_new(col.a[0], col.a[1], col.a[2]);
+		}
+		else if (!ft_strcmp(temp->tag, "surface"))
+			get_surface(temp, mat);
+		index++;
+	}
+	return (TRUE);
+}
+
+int	get_material(t_xml_node *node, t_material *mat)
+{
+	t_xml_attr	*attr;
+	int			index;
+
+	index = 0;
+	while (index < node->attributes.size)
+	{
+		attr = &node->attributes.list[index];
+		if (!ft_strcmp(attr->key, "pattern") && \
+		!ft_strcmp(attr->value, "striped"))
+			mat->pattern.pattern_id = STRIPED;
+		else if (!ft_strcmp(attr->key, "pattern") && \
+		!ft_strcmp(attr->value, "grid"))
+			mat->pattern.pattern_id = GRID;
+		else if (!ft_strcmp(attr->key, "perlin") && \
+		!ft_strcmp(attr->value, "true"))
+			mat->pattern.pattern_perlin = TRUE;
+		index++;
+	}
+	get_material_details(node, mat);
+	return (TRUE);
+}
