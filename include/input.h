d26f0d476fc01f2f6b4ae60ac14712ca18478b0a
Jaro Juntunen <jjuntune@c2r4p6.hive.fi>
2023-01-19 16:26:53 +0200

    norminetting imput.h file

--- include/input.h
+++ include/input.h
@@ -6,7 +6,7 @@
 /*   By: jjuntune <jjuntune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 13:33:14 by mmakinen          #+#    #+#             */
-/*   Updated: 2023/01/02 16:16:58 by jjuntune         ###   ########.fr       */
+/*   Updated: 2023/01/19 16:26:24 by jjuntune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ int			get_camera(t_xml_node *node, t_cam *cam);
 /* Get obj information */
 int			get_object(t_xml_node *node, t_object *obj);
 int			get_material(t_xml_node *node, t_material *mat, t_object *obj);
-int			get_material_details(t_xml_node *node, t_material *mat, t_object *obj);
+int			get_material_details(t_xml_node *node,
+				t_material *mat, t_object *obj);
 void		get_surface(t_xml_node *surface, t_material *mat);
 int			get_motion(t_xml_node *node, t_motion_blur *motion);
 int			get_rotation(t_xml_node *node, t_object *obj);
