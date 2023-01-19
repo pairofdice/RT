dd4ed4b199c6bc34d28372257bf2064da24c9f6b
Jaakko Saarinen <jsaarine@student.hive.fi>
2023-01-19 14:16:35 +0200

    set_blocked_lghts

--- src/image_render.c
+++ src/image_render.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   image_render.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjuntune <jjuntune@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jsaarine <jsaarine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 17:56:58 by jjuntune          #+#    #+#             */
-/*   Updated: 2023/01/05 12:43:34 by jjuntune         ###   ########.fr       */
+/*   Updated: 2023/01/19 14:16:05 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,23 @@ static void	aa_help(t_anti_aliasing *aa, t_main *main, int pixel_x)
 	aa->color = tuple_add(aa->color, color_at(&main->scene, &main->ray));
 	vec_free(&main->ray.xs.vec);
 	aa->sub_pixel++;
+}
+
+static void set_blocked_lights(t_main *main)
+{
+	t_light	light;
+	t_ray	ray;
+	size_t	i;
+
+	i = 0;
+	while (i < main->scene.lights.len)
+	{
+		light = *(t_light *) vec_get(&main->scene.lights, i);
+		ray = ray_new_no_malloc(main->scene.cam.pos, light.direction);
+		light.blocked = is_shadowed(&main->scene, &light, &main->scene.cam.pos, &main->ray.hit);
+		i++;
+	}
+	
 }
 
 /*
